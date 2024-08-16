ef5a7e3b0ebe6d1065872e4ea70d7e6931fe1bf7
Brian0o0 <nguyenhpse160876@fpt.edu.vn>
2024-08-17 00:17:22 +0700

    CRUD Category

---
+++ XuongMayBE.API/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using XuongMay.Contract.Repositories.Entity;
+using XuongMay.Contract.Services.Interface;
+using XuongMay.Core.Base;
+using XuongMay.Services.Service;
+
+namespace XuongMayBE.API.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService) {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getAllCategory")]
+        public async Task<IActionResult> GetAllCategory() {
+            IList<Category> categories = await _categoryService.GetAll();
+            if(categories == null){
+                return BadRequest();
+            }
+            return Ok(BaseResponse<IList<Category>>.OkResponse(categories));
+        }
+
+        [HttpGet("getByCategoryId/{id}")]
+        public async Task<IActionResult> GetCategoryById(object id)
+        {
+            Category category = await _categoryService.GetCategoryById(id);
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            return Ok(BaseResponse<Category>.OkResponse(category));
+        }
+    }
+}
