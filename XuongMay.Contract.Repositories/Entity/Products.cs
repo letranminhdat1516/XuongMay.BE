9d192de84a7a2251ca36fb2f648ab8b9d2931a0f
Brian0o0 <nguyenhpse160876@fpt.edu.vn>
2024-08-16 17:15:20 +0700

    Database

--- XuongMay.Contract.Repositories/Entity/Products.cs
+++ XuongMay.Contract.Repositories/Entity/Products.cs
@@ -1,14 +1,23 @@
-﻿using XuongMay.Core.Base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using XuongMay.Core.Base;
 
 namespace XuongMay.Contract.Repositories.Entity
 {
+    [Table("Products")]
     public class Products : BaseEntity
     {
-        public String ProductName { get; set; } = string.Empty;
-        public String ProductDescription { get; set; } = string.Empty;
-        public Decimal ProductPrice { get; set; }
-        public String? CategoryId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public float ProductPrice { get; set; }
+        public string? CategoryId { get; set; }
 
-        public virtual Category? Category { get; set; }
+        // Navigation property
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
     }
 }
+
+
+
+
